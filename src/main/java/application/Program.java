7f27adbae89178c799436b255c6cba0ea0f4f9bf
Perkles <walisoncastor@gmail.com>
2023-07-05 14:54:31 -0300

    Exercise completed

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Program {
 
@@ -13,11 +14,31 @@ public class Program {
         list.add("Alex");
         list.add("Bob");
         list.add("Anna");
+        list.add(2, "Marco");
 
+        System.out.println(list.size());
         for (String x : list){
             System.out.println(x);
         }
+        System.out.println("------------------");
+        list.removeIf(x -> x.charAt(0) == 'M');
+        for (String x : list){
+            System.out.println(x);
+        }
+        System.out.println("------------------");
+        System.out.println("Index of Bob: " + list.indexOf("Bob"));
+        System.out.println("Index of Marco: " + list.indexOf("Marco"));
+        System.out.println("------------------");
+        List<String> result = list.stream().filter(x -> x.charAt(0) == 'A').collect(Collectors.toList());
+        for (String x : result) {
+            System.out.println(x);
+        }
+        System.out.println("------------------");
+        String name = list.stream().filter(x -> x.charAt(0) == 'A').findFirst().orElse(null);
+        String name1 = list.stream().filter(x -> x.charAt(0) == 'J').findFirst().orElse(null);
 
+        System.out.println(name);
+        System.out.println(name1);
     }
 
 }
